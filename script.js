cd505bc18ef27db2154674c95a18e9fef48c94e9
Peter Gryllis <petergryllis@riacci.com>
2022-01-05 17:03:43 +1100

    add html code for contenteditable

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 var today = moment();
 $("#1a").text(today.format('LLLL'));
+
+var taskItemFILL = $('#taskItem');
+
+function handletaskentry(event){
+    event.preventDefault();
+
+    var taskENTRY = $('input [name="task-input"]').val();
+
+    if(!taskENTRY) {
+        console.log('No task entered');
+        return;
+    }
+
+    var taskEntryElement = $(
+        '<li class="flex-row justify-space-between align-center p-2 bg-light text-dark">'
+    );
+    taskEntryElement.text(taskENTRY);
+
+    taskItemFILL.append(taskEntryElement);
+
+    $('input[name="task-input"]').val('');
+}
