16bc874f44af390c8e9cc5666995b3191bf94e3d
Peter Gryllis <petergryllis@riacci.com>
2022-01-07 19:13:07 +1100

    update js with functions

--- script.js
+++ script.js
@@ -14,18 +14,18 @@ var block4pm = "4:00:00 pm";
 var block5pm = "5:00:00 pm";
 
 
-var Hour9am = document.querySelector("#9am");
-var Hour10am = document.querySelector("#10am");
-var Hour11am = document.querySelector("#11am");
-var Hour12pm = document.querySelector("#12pm");
-var Hour1pm = document.querySelector("#1pm");
-var Hour2pm = document.querySelector("#2pm");
-var Hour3pm = document.querySelector("#3pm");
-var Hour4pm = document.querySelector("#4pm");
-var Hour5pm = document.querySelector("#5pm");
+var Hour9am = document.getElementById("#9am");
+var Hour10am = document.getElementById("#10am");
+var Hour11am = document.getElementById("#11am");
+var Hour12pm = document.getElementById("#12pm");
+var Hour1pm = document.getElementById("#1pm");
+var Hour2pm = document.getElementById("#2pm");
+var Hour3pm = document.getElementById("#3pm");
+var Hour4pm = document.getElementById("#4pm");
+var Hour5pm = document.getElementById("#5pm");
 
 if(currentTime.isBetween(block9am, block10am)) {
-    Hour9am.setAttribute ("style", "present"); }
+    Hour9am.setAttribute("style", "present"); }
     else if(currentTime.isBefore(block9am))
         Hour9am.setAttribute ("style", "future");
         else {
@@ -55,4 +55,28 @@ if(currentTime.isBetween(block12pm, block1pm)) {
         Hour12pm.setAttribute ("style", "future");
         else {
             Hour12pm.setAttribute("style", "past");
+        }
+
+if(currentTime.isBetween(block1pm, block2pm)) {
+    Hour1pm.setAttribute ("style", "present"); }
+    else if(currentTime.isBefore(block2pm))
+        Hour1pm.setAttribute ("style", "future");
+        else {
+            Hour1pm.setAttribute("style", "past");
+        }
+
+if(currentTime.isBetween(block2pm, block3pm)) {
+    Hour2pm.setAttribute ("style", "present"); }
+    else if(currentTime.isBefore(block3pm))
+        Hour2pm.setAttribute ("style", "future");
+        else {
+            Hour2pm.setAttribute("style", "past");
+        }
+
+if(currentTime.isBetween(block2pm, block3pm)) {
+    Hour2pm.setAttribute ("style", "present"); }
+    else if(currentTime.isBefore(block3pm))
+        Hour2pm.setAttribute ("style", "future");
+        else {
+            Hour2pm.setAttribute("style", "past");
         }
