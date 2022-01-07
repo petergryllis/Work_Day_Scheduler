3617bc0e071383ec36d1f751edd180e2f1357b77
Peter Gryllis <petergryllis@riacci.com>
2022-01-07 18:10:21 +1100

    update variables in js to reflect time blocks

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 var today = moment();
 $("#1a").text(today.format('LLLL'));
 
+var presentBlock = document.querySelector(".present");
+
+var pastBlock = document.querySelector(".past");
+
+var presentBlock = document.querySelector(".present");
+
